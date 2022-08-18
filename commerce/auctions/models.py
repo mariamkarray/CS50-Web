77f07bdaea0f8b3c0680d841744fcc28583aefc0
mariamkarray <54550801+mariamkarray@users.noreply.github.com>
2022-08-18 10:29:11 +0200

    create listing

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class AuctionListing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=120)
+    bid = models.IntegerField(max_length=10000)
+    image =  models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Bids(models.Model):
+    pass
+
+class Comments(models.Model):
+    pass
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -1,15 +1,35 @@
+from cProfile import label
+from logging import PlaceHolder
+from tkinter import Widget
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
+from django.forms import ModelForm
+import auctions
+from .models import User, AuctionListing
 
-from .models import User
-
+class ListingForm(ModelForm):
+    class Meta:
+        model = AuctionListing
+        fields = "__all__"
+        labels = {
+            'title': '',
+            'description': '',
+            'bid': '',
+            'image': ''
+        }
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Title'}),
+            'description': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Description'}),
+            'bid': forms.NumberInput(attrs={'class':'form-control', 'placeholder':'Starting Bid'}),
+            'image': forms.URLInput(attrs={'class':'form-control', 'placeholder':'URL for The Image of Listing and/or a Category (optional)'})
+        }
 
 def index(request):
     return render(request, "auctions/index.html")
-
 
 def login_view(request):
     if request.method == "POST":
@@ -61,3 +81,25 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+def listing(request):
+    # variable fot tracking if the form was submitted or not
+    submitted = False
+    if request.method == "POST":
+        # request.POST takes whatever was in the input and passes it to the form
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # redirect back to the page itself
+            return HttpResponseRedirect('/listing?submitted=True')
+    else:
+        form = ListingForm
+        # the responseRedirect passes the submitted variable into the get request 
+        # if it did that that means the user submitted the form
+        if 'submitted' in request.GET:
+            submitted = True     
+
+    return render(request, "auctions/listing.html", {
+        "form" : form,
+        'submitted' : submitted
+    })
